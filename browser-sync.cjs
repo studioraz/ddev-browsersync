43039b4c0dde0abaf75caef9c11dbd57bca3b316
Itay Raz <itay@studioraz.co.il>
2025-06-03 23:18:20 +0300

    Update browser-sync.cjs

--- browser-sync.cjs
+++ browser-sync.cjs
@@ -2,16 +2,30 @@
 let docroot = process.env.DDEV_DOCROOT;
 let filesdir = process.env.DDEV_FILES_DIR;
 let url = process.env.DDEV_HOSTNAME;
-let nonSslUrl = process.env.DDEV_PRIMARY_URL.replace( /^https:/, 'http:' )
+let nonSslUrl = process.env.DDEV_PRIMARY_URL.replace(/^https:/, 'http:')
 
 if (filesdir === "") {
     filesdir = null
 }
 
 module.exports = {
-
-    files: [docroot, "app", "resources/**/*.php"],
-    ignore: ["node_modules", filesdir, "vendor"],
+    // Watch only these file extensions under app/code and app/design:
+    files: [
+        "app/code/**/*.js",
+        "app/code/**/*.xml",
+        "app/code/**/*.css",
+        "app/code/**/*.phtml",
+        "app/code/**/*.html",
+        "app/design/**/*.js",
+        "app/design/**/*.xml",
+        "app/design/**/*.css",
+        "app/design/**/*.phtml",
+        "app/design/**/*.html"
+    ],
+    ignore: [
+        "node_modules",
+        filesdir, "vendor"
+    ],
     open: false,
     ui: false,
     server: false,
